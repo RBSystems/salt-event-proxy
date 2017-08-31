b7c8ff3d19f0c8fb71c8eae463613a34e87bc0cc
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-08-31 00:08:01 -0600

    adding dhcp stuff

--- server.go
+++ server.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 
 	"github.com/byuoitav/event-router-microservice/eventinfrastructure"
@@ -61,6 +65,120 @@ func sendSaltEvent() {
 	}
 }
 
+//a true means DHCP is turned on
+func getDHCPStatus() (bool, error) {
+	//we have to look at the contents of the /etc/dhcpcd.conf file to see if it's dhcp or not.
+
+	file := "/etc/dhcpcd.conf"
+
+	contents, err := ioutil.ReadFile(file)
+	if err != nil {
+		errString := fmt.Sprintf("There was an issue reading the dhcpcd file: %v", err.Error())
+		log.Printf(errString)
+		return false, errors.New(errString)
+	}
+
+	re, err := regexp.Compile(`(?m)^static ip_address`)
+	if err != nil {
+		return false, errors.New(fmt.Sprintf("There was an issue compiling the regex : %v", err.Error()))
+	}
+
+	matches := re.Match(contents)
+
+	fmt.Printf("matches: %v", matches)
+
+	return !matches, nil
+}
+
+func canToggle() bool {
+	//we assume that when the dhcpcd.conf file was created it was copied and created a .other file - if there's no .other file, nothing we can do.
+	file := "/etc/dhcpcd.conf"
+	otherFile := "/etc/dhcpcd.conf.other"
+
+	if _, err := os.Stat(otherFile); os.IsNotExist(err) {
+		str := fmt.Sprintf("There isn't an other file, and we can't toggle DHCP")
+		log.Printf(str)
+		return false
+	}
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		str := fmt.Sprintf("There isn't an file, and we can't toggle DHCP")
+		log.Printf(str)
+		return false
+	}
+
+	return true
+}
+
+func toggleDHCP() error {
+	//we assume that when the dhcpcd.conf file was created it was copied and created a .other file - if there's no .other file, nothing we can do.
+	file := "/etc/dhcpcd.conf"
+	otherFile := "/etc/dhcpcd.conf.other"
+
+	if !canToggle() {
+		return errors.New("Can't toggle dhcp, files necessary aren't present")
+	}
+
+	//do the rename
+	temp := "/etc/dhcpcd.conf.temp"
+	err := os.Rename(file, temp)
+	if err != nil {
+		errStr := fmt.Sprintf("Couldn't rename file %v", err.Error())
+		log.Printf("%v", errStr)
+		return errors.New(errStr)
+	}
+
+	err = os.Rename(otherFile, file)
+	if err != nil {
+		errStr := fmt.Sprintf("Couldn't rename file %v", err.Error())
+		log.Printf("%v", errStr)
+		return errors.New(errStr)
+	}
+
+	err = os.Rename(temp, otherFile)
+	if err != nil {
+		errStr := fmt.Sprintf("Couldn't rename file %v", err.Error())
+		log.Printf("%v", errStr)
+		return errors.New(errStr)
+	}
+
+	_, err = exec.Command("sh", "-c", `sudo systemctl restart dhcpcd`).Output()
+	if err != nil {
+		errStr := fmt.Sprintf("Problem restarting the service: %v", err.Error())
+		log.Printf(errStr)
+		return errors.New(errStr)
+	}
+
+	return nil
+}
+
+func getStaticIP() string {
+	//we can check if it's dhcp, if it is we get the .other file, look for the ip address there.
+	return ""
+}
+
+func ToggleDHCP(context echo.Context) error {
+	err := toggleDHCP()
+	if err != nil {
+		return context.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return GetDHCPState(context)
+}
+
+func GetDHCPState(context echo.Context) error {
+
+	val, err := getDHCPStatus()
+	if err != nil {
+		return context.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	if val {
+		return context.JSON(http.StatusOK, "static")
+	}
+	return context.JSON(http.StatusOK, "dhcp")
+
+}
+
 func reboot(context echo.Context) error {
 	log.Printf("Rebooting")
 
@@ -99,6 +217,8 @@ func main() {
 	router.POST("/event/:type/:cause", QueueEvent)
 	router.GET("/reboot", reboot)
 	router.GET("/dockerStatus", getDockerStatus)
+	router.GET("/dhcp", GetDHCPState)
+	router.PUT("/dhcp", ToggleDHCP)
 
 	server := http.Server{
 		Addr:           port,
